bdb125c4200741e26dfb5a2b968a07bc08a36d50
LuisMRincn <74801550+LuisMRincn@users.noreply.github.com>
2020-11-20 21:28:23 -0400

    Add files via upload

---
+++ PatronSingletonC#/Singleton/Singleton/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton Ejemplo1 = Singleton.Validacion();
+
+            Singleton Ejemplo2 = Singleton.Validacion();
+
+
+            Console.WriteLine("10 Dolares en peso = "+Ejemplo1);
+            Console.WriteLine(Ejemplo2);
+
+            if(Ejemplo1 == Ejemplo2)
+            {
+                Console.WriteLine("Funciona");
+            }
+            Console.ReadKey();
+
+            
+        }
+    }
+}
---
+++ PatronSingletonC#/Singleton/Singleton/Singleton.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton
+{
+    public class Singleton
+    {
+        private int Dolar = 10;
+        private int resultado;
+
+        
+        private Singleton() {
+            resultado = Dolar * 58;
+        }
+
+        private static Singleton Ejemplo;
+
+        public static Singleton Validacion()
+        {
+            if (Ejemplo == null)
+            {
+
+                Ejemplo = new Singleton();
+
+            }
+            return Ejemplo;
+        }
+        public override string ToString()
+        {
+            return resultado.ToString();
+        }
+
+        
+    }
+}
